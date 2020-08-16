24afb59c168b9c6c302c0b27f708335a41a161df
jorgeruvalcaba <jorgeruvalcabav@gmail.com>
2020-08-16 11:05:19 -0500

    Adding GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,8 +64,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-148404893-1`,
       },
     },
     {
